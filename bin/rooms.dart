989cd0baed0f831b953973d98b2bc8c9b85931fc
probuse <nprobuse@gmail.com>
2021-06-17 13:57:00 +0300

    get input from user

--- bin/rooms.dart
+++ bin/rooms.dart
@@ -4,39 +4,5 @@ import 'package:args/args.dart';
 import 'package:rooms/rooms.dart' as rooms;
 
 void main(List<String> arguments) {
-  // var parser = ArgParser();
-  // parser.addOption('name', abbr: 'n');
-
-  // var results = parser.parse(arguments);
-
-  // if (results['name'] != null) {
-  //   var message = rooms.addRoom(results['name']);
-  //   print(message);
-
-  //   if (results.rest.isNotEmpty) {
-  //     for (var name in results.rest) {
-  //       rooms.addRoom(name);
-  //     }
-  //   }
-  // }
-  String name, age, gender;
-  while (true) {
-    stdout.write('Enter your name : \n');
-    name = stdin.readLineSync();
-    stdout.write('Enter your gender : \n');
-    gender = stdin.readLineSync();
-    stdout.write('Enter your Age : \n');
-    age = stdin.readLineSync();
-
-    print(name);
-    print(gender);
-    print(age);
-
-    exit(0);
-    // if (n == 3) {
-    //   exit(0);
-    // }
-  }
-
-  // print('rooms are ${rooms.rooms}');
+  rooms.getUserInformation();
 }
--- lib/rooms.dart
+++ lib/rooms.dart
@@ -1,6 +1,50 @@
-// TODO we shall use this when adding rooms
-// List<String> rooms = [];
-// String addRoom(String roomName) {
-//   rooms.add(roomName);
-//   return '$roomName added successfully';
-// }
+import 'dart:core';
+
+import 'dart:io';
+
+void getUserInformation() {
+  String name;
+  String emailAddress;
+  String gender;
+  String country;
+  String favoriteFood;
+  String hobby;
+  String friend;
+  String favoriteCar;
+  while (true) {
+    stdout.write('Enter your name : \n');
+    name = stdin.readLineSync();
+
+    stdout.write('Enter your email address : \n');
+    emailAddress = stdin.readLineSync();
+
+    stdout.write('Enter your gender : \n');
+    gender = stdin.readLineSync();
+
+    stdout.write('Enter your country : \n');
+    country = stdin.readLineSync();
+
+    stdout.write('Enter your favorite food : \n');
+    favoriteFood = stdin.readLineSync();
+
+    stdout.write('Enter your hobby : \n');
+    hobby = stdin.readLineSync();
+
+    stdout.write("Enter your friend's name : \n");
+    friend = stdin.readLineSync();
+
+    stdout.write('What is your favorite car : \n');
+    favoriteCar = stdin.readLineSync();
+
+    print('My name is $name');
+    print('My email address is $emailAddress');
+    print('My gender is $gender');
+    print('My country is $country');
+    print('My favorite Food is $favoriteFood');
+    print('My hobby is $hobby');
+    print('My friend is $friend');
+    print('My favorite Car is $favoriteCar');
+
+    exit(0);
+  }
+}
