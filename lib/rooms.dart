ac307bf2a78ebec3e9bd1c516fbc6e3465302124
probuse <nprobuse@gmail.com>
2021-06-17 19:22:18 +0300

    pick user age, limit to 3 tries if an integer is not entered, code clean up

--- lib/rooms.dart
+++ lib/rooms.dart
@@ -2,49 +2,92 @@ import 'dart:core';
 
 import 'dart:io';
 
+String name;
+String emailAddress;
+String gender;
+String country;
+String favoriteFood;
+String hobby;
+String friend;
+String favoriteCar;
+int age;
+
 void getUserInformation() {
-  String name;
-  String emailAddress;
-  String gender;
-  String country;
-  String favoriteFood;
-  String hobby;
-  String friend;
-  String favoriteCar;
   while (true) {
-    stdout.write('Enter your name : \n');
-    name = stdin.readLineSync();
+    // get user's name
+    name = getUserResponse('Enter your name');
 
-    stdout.write('Enter your email address : \n');
-    emailAddress = stdin.readLineSync();
+    // get user's email address
+    emailAddress = getUserResponse('Enter your email address');
 
-    stdout.write('Enter your gender : \n');
-    gender = stdin.readLineSync();
+    // get user gender
+    gender = getUserResponse('Enter your gender');
 
-    stdout.write('Enter your country : \n');
-    country = stdin.readLineSync();
+    // get user's country
+    country = getUserResponse('Enter your country');
 
-    stdout.write('Enter your favorite food : \n');
-    favoriteFood = stdin.readLineSync();
+    // get user's favorite food
+    favoriteFood = getUserResponse('Enter your favorite food');
 
-    stdout.write('Enter your hobby : \n');
-    hobby = stdin.readLineSync();
+    // get user's hobby
+    hobby = getUserResponse('Enter your hobby');
 
-    stdout.write("Enter your friend's name : \n");
-    friend = stdin.readLineSync();
+    // get user's friends
+    friend = getUserResponse("Enter your friends' name");
 
-    stdout.write('What is your favorite car : \n');
-    favoriteCar = stdin.readLineSync();
+    // get user's favorite car
+    favoriteCar = getUserResponse('What is your favorite car');
 
-    print('My name is $name');
-    print('My email address is $emailAddress');
-    print('My gender is $gender');
-    print('My country is $country');
-    print('My favorite Food is $favoriteFood');
-    print('My hobby is $hobby');
-    print('My friend is $friend');
-    print('My favorite Car is $favoriteCar');
+    // get user's age
+    age = getUserAge();
+
+    printResults();
 
     exit(0);
   }
+}
+
+String getUserResponse(String question) {
+  stdout.write('$question: \n');
+  return stdin.readLineSync();
+}
+
+/// returns a list of foods from a string of foods separated by space
+List<String> getFavoriteFoods(String favoriteFoodString) {
+  return favoriteFoodString.split(' ');
+}
+
+var retries = 3;
+int getUserAge() {
+  stdout.write('Enter your age: \n');
+  var response = stdin.readLineSync();
+  int age;
+  try {
+    age = int.parse(response);
+    return age;
+  } catch (e) {
+    if (retries > 0) {
+      retries -= 1;
+      print('Invalid age, you have $retries retries left');
+      return getUserAge();
+    } else {
+      print(
+          'Invalid, you have used all your retries, your age wont be captured');
+      return null;
+    }
+  }
+}
+
+void printResults() {
+  print('**********Results*************');
+  print('My name is $name');
+  print('My email address is $emailAddress');
+  print('My gender is $gender');
+  print('My country is $country');
+  print('My favorite Food is $favoriteFood');
+  print('My hobby is $hobby');
+  print('My friend is $friend');
+  print('My favorite Car is $favoriteCar');
+  print('My age is $age');
+  print('*********Results**************');
 }
